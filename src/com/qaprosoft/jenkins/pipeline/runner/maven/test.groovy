7435c9cbcc8bb260cd9bc66da84465ab73599f00
vdelendik <vadim.delendik@gmail.com>
2020-05-20 02:29:31 +0300

    reused log in test

--- src/com/qaprosoft/jenkins/pipeline/runner/maven/test.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/maven/test.groovy
@@ -1,16 +1,18 @@
+package com.qaprosoft.jenkins.pipeline.runner.maven
 // See https://github.com/jenkinsci/workflow-cps-global-lib-plugin
 
 // The call(body) method in any file in workflowLibs.git/vars is exposed as a
 // method with the same name as the file.
 def call(body) {
     def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-    stage 'checkout'
-    node {
-        println body.dump()
-        println config.dump()
+    log.info 'Starting'
+//    body.resolveStrategy = Closure.DELEGATE_FIRST
+//    body.delegate = config
+//    body()
+//    stage 'checkout'
+//    node {
+//        println body.dump()
+//        println config.dump()
 //        checkout scm
 //        stage 'main'
 //        docker.image(config.environment).inside {
@@ -18,5 +20,5 @@ def call(body) {
 //        }
 //        stage 'post'
 //        sh config.postScript
-    }
+//    }
 }
