7eb64a89a3c2b8e488bbd7ed7800bcbb6d796eac
vdelendik <vadim.delendik@gmail.com>
2020-05-20 13:04:55 +0300

    added test2

---
+++ vars/test2.groovy
@@ -0,0 +1,27 @@
+
+// See https://github.com/jenkinsci/workflow-cps-global-lib-plugin
+
+// The call(body) method in any file in workflowLibs.git/vars is exposed as a
+// method with the same name as the file.
+
+def call(body) {
+    def config = [:]
+
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    stage 'checkout'
+    node {
+//        println "Starting1"
+//        log.info "Starting2"
+//        println body.dump()
+//        println config.dump()
+//        checkout scm
+        stage 'main'
+//        docker.image(config.environment).inside {
+//            sh config.mainScript
+//        }
+        stage 'post'
+//        sh config.postScript
+    }
+}
