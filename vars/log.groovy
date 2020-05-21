46c08ab8b4ab393aa14531da4398deb1f335fa3d
vdelendik <vadim.delendik@gmail.com>
2020-05-22 01:48:40 +0300

    added enums to globals

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,3 +1,41 @@
+/*******************************************************************************
+ * Copyright 2013-2020 QaProSoft (http://www.qaprosoft.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+public static enum LogLevel {
+
+    DEBUG(1),
+    INFO(2),
+    WARN(3),
+    ERROR(4),
+
+    final int value
+    LogLevel(int value) {
+        this.value = value
+    }
+}
+
+public enum ContextType {
+    JOB_DSL,
+    PIPELINE
+}
+
+def context
+LogLevel pipelineLogLevel
+ContextType contextType
+
 def info(message) {
     echo "INFO: ${message}"
 }
