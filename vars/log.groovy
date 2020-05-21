0fcda01c088aea8da83c4cda5bd001f8fd19dccb
vdelendik <vadim.delendik@gmail.com>
2020-05-22 02:10:38 +0300

    import groovy.transform.Field

--- vars/log.groovy
+++ vars/log.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *******************************************************************************/
-
+import groovy.transform.Field
 import static com.qaprosoft.jenkins.Utils.*
  
 public enum LogLevel {
