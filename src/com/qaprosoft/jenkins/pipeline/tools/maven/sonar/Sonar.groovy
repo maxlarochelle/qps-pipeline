a6988c153519e5a4f8936eb30b09f586b8130890
sandinosanchez <sandino320@gmail.com>
2020-05-21 23:31:42 -0300

    added import

--- src/com/qaprosoft/jenkins/pipeline/tools/maven/sonar/Sonar.groovy
+++ src/com/qaprosoft/jenkins/pipeline/tools/maven/sonar/Sonar.groovy
@@ -6,7 +6,7 @@ import hudson.plugins.sonar.SonarGlobalConfiguration
 import com.qaprosoft.jenkins.pipeline.tools.maven.Maven
 import com.qaprosoft.jenkins.pipeline.tools.scm.ISCM
 import com.qaprosoft.jenkins.pipeline.tools.scm.github.GitHub
-
+import static com.qaprosoft.jenkins.pipeline.Executor.*
 import static com.qaprosoft.jenkins.Utils.*
 
 @Mixin(Maven)
